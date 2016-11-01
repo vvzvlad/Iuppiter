e96ae27f236833c2a8719c467d9714af12419e44
vvzvlad <vvzvlad@gmail.com>
2016-11-02 02:11:57 +0300

    update avrcore

--- avrcore/tarsha.sh
+++ avrcore/tarsha.sh
@@ -1,3 +1,4 @@
-tar -zcvf IupputerAVRCore-0.95.tar.gz ./IupputerAVRCore-0.95/*
+cd ./IupputerAVRCore-0.95/
+tar -zcvf IupputerAVRCore-0.95.tar.gz ./*
 shasum -a 256 IupputerAVRCore-0.95.tar.gz
 stat -F IupputerAVRCore-0.95.tar.gz 
