bb6c98b34c78b1c7d08be8d4a0c2a296f78e1658
vvzvlad <vvzvlad@gmail.com>
2016-11-02 03:48:48 +0300

    update core's

---
+++ arduino_ide_boards/sha.sh
@@ -0,0 +1,2 @@
+shasum -a 256 esp8266-2.3.0.tar.gz
+shasum -a 256 IupputerAVRCore-0.95.tar.gz
--- arduino_ide_boards/tar.sh
+++ arduino_ide_boards/tar.sh
@@ -7,8 +7,6 @@ rm esp8266-2.3.0.tar.gz
 cd ./esp8266-2.3.0/
 tar -zcvf ../esp8266-2.3.0.tar.gz ./
 cd ..
-shasum -a 256 esp8266-2.3.0.tar.gz
-stat -F esp8266-2.3.0.tar.gz
 
-shasum -a 256 IupputerAVRCore-0.95.tar.gz
+stat -F esp8266-2.3.0.tar.gz
 stat -F IupputerAVRCore-0.95.tar.gz 
