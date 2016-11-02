0e8f90ddc64c3398bfa05893d7e63f815db74ebc
vvzvlad <vvzvlad@gmail.com>
2016-11-02 03:14:08 +0300

    add esp8266 core

--- avrcore/tarsha.sh
+++
@@ -1,6 +0,0 @@
-rm IupputerAVRCore-0.95.tar.gz
-cd ./IupputerAVRCore-0.95/
-tar -zcvf ../IupputerAVRCore-0.95.tar.gz ./
-cd ..
-shasum -a 256 IupputerAVRCore-0.95.tar.gz
-stat -F IupputerAVRCore-0.95.tar.gz 
